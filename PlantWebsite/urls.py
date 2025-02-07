f1fc32bbede4c56db94b01ae8ee4ca36c0a64f00
Shani871 <chauhan145@gmail.com>
2025-02-07 10:29:14 +0530

    Backend

--- PlantWebsite/urls.py
+++ PlantWebsite/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('portfolio/', views.portfolio, name='portfolio'),
     path('checkout/', views.checkout, name='checkout'),
     path('blog/', views.blog, name='blog'),
+path('index/', views.index, name='index'),
+    path('detail/<int:id>/', views.detail, name='detail'),
+    path('checkout/', views.checkout, name='checkout'),
 
 ]
--- PlantWebsite/views.py
+++ PlantWebsite/views.py
@@ -1,4 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from PlantWebsite.models import Product, Order
+
 
 # Create your views here.
 
@@ -10,7 +14,45 @@ def contact(request):
     return render(request, 'contact.html')
 def portfolio(request):
     return render(request, 'portfolio.html')
-def checkout(request):
-    return render(request, 'checkout.html')
+
 def blog(request):
     return render(request, 'blog.html')
+def index(request):
+    product_object = Product.objects.all()
+
+
+    # search code
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_object = product_object.filter(title__icontains=item_name)
+
+    # paginator code
+    paginator = Paginator(product_object, 10)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+
+def detail(request,id):
+    product_object = Product.objects.get(id=id)
+    return  render(request,'shop/detail.html',{'product_object':product_object})
+
+
+
+
+
+def checkout(request):
+
+    if request.method == "POST":
+        items = request.POST.get('items',"")
+        name = request.POST.get('name',"")
+        email = request.POST.get('email',"")
+        address = request.POST.get('address',"")
+        city = request.POST.get('city',"")
+        state = request.POST.get('state',"")
+        zipcode = request.POST.get('zipcode',"")
+        total = request.POST.get('total',"")
+        order  = Order(item=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
+        order.save()
+    return render(request,'shop/checkout.html')
